a215c8a2e0ce200eec099a9bae4bf49018f10bf7
AbdullahJanKhan1 <abdullahi.khan@tabaarakict.org>
2023-01-10 15:33:38 +0500

    updated certs generation

--- certs/get-certs.sh
+++ certs/get-certs.sh
@@ -11,8 +11,8 @@ openssl x509 -in ca-cert.pem -noout -text
 # 2. Generate web server's private key and certificate signing request (CSR)
 openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -subj "/C=FR/ST=Ile de France/L=Paris/O=Server TLS/OU=Server/CN=*.tls/emailAddress=tls@gmail.com"
 
-# Remember that when we develop on localhost, It’s important to add the IP:0.0.0.0 as an Subject Alternative Name (SAN) extension to the certificate.
-echo "subjectAltName=DNS:*.tls,DNS:localhost,IP:0.0.0.0" > server-ext.cnf
+# Remember that when we develop on localhost, It’s important to add the IP:0.tcp.in.ngrok.io as an Subject Alternative Name (SAN) extension to the certificate.
+echo "subjectAltName=DNS:*.tls,DNS:grpc.faras.com,IP:129.151.80.251" > server-ext.cnf
 # Or you can use localhost DNS and grpc.ssl_target_name_override variable
 # echo "subjectAltName=DNS:localhost" > server-ext.cnf
 
@@ -25,8 +25,8 @@ openssl x509 -in server-cert.pem -noout -text
 # 4. Generate client's private key and certificate signing request (CSR)
 openssl req -newkey rsa:4096 -nodes -keyout client-key.pem -out client-req.pem -subj "/C=FR/ST=Alsace/L=Strasbourg/O=PC Client/OU=Computer/CN=*.client.com/emailAddress=client@gmail.com"
 
-# Remember that when we develop on localhost, It’s important to add the IP:0.0.0.0 as an Subject Alternative Name (SAN) extension to the certificate.
-echo "subjectAltName=DNS:*.client.com,IP:0.0.0.0" > client-ext.cnf
+# Remember that when we develop on localhost, It’s important to add the IP:0.tcp.in.ngrok.io as an Subject Alternative Name (SAN) extension to the certificate.
+echo "subjectAltName=DNS:grpc.faras.com,IP:129.151.80.251" > client-ext.cnf
 
 # 5. Use CA's private key to sign client's CSR and get back the signed certificate
 openssl x509 -req -in client-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -extfile client-ext.cnf
